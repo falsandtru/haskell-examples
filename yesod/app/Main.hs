1c78b5f1cc6add80101b78bdd12a4952cacf4458
falsandtru <falsandtru@users.noreply.github.com>
2019-03-30 16:56:30 +0900

    Add yesod example

---
+++ yesod/app/Main.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+module Main where
+
+import           Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+main :: IO ()
+main = warp 80 HelloWorld
